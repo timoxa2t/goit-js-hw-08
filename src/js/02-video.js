f46d081b8a088c3a6a10347e6026a79c8412bf1f
timoxa2t <alex2t@ukr.net>
2022-08-08 12:20:30 +0300

    localStorage & validity fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,16 @@
 import Player from '@vimeo/player'
 
 const throttle = require('lodash.throttle');
+const VIDEOPLAYER_CURRENT_TIME = "videoplayer-current-time"
 
 const videoEl = document.querySelector("#vimeo-player")
 
 const player = new Player(videoEl);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+const time = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME)
+
+time && player.setCurrentTime(time)
 
 player.on('timeupdate', throttle(({duration, percent, seconds}) => {
-    localStorage.setItem("videoplayer-current-time", seconds) 
+    localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, seconds) 
 }, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,13 @@
 const trottle = require("lodash.throttle")
 const formEl = document.querySelector(".feedback-form")
 
+const FEEDBACK_FORM_STATE = "feedback-form-state"
+
 const emailEl = document.querySelector(`input[name="email"]`)
 const messageEl = document.querySelector(`textarea[name="message"]`)
 const submitBtn = document.querySelector(`button[type="submit"]`)
 
-let formData = Object(JSON.parse(localStorage.getItem("feedback-form-state")))
+let formData = Object(JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE)))
 
 
 emailEl.value = formData.email || ""
@@ -13,15 +15,17 @@ messageEl.value = formData.message || ""
 
 formEl.addEventListener("input", trottle(({target}) => {
     formData[target.name] = target.value
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData))
 }, 500))
 
 submitBtn.addEventListener("click", (event) => {
-    event.preventDefault(); 
-    console.log(formData)
-    formData = {}
-    emailEl.value = ""
-    messageEl.value = ""
-    localStorage.clear()
+    if(emailEl.checkValidity() && messageEl.checkValidity()) {
+        event.preventDefault(); 
+        console.log(formData)
+        formData = {}
+        emailEl.value = ""
+        messageEl.value = ""
+        localStorage.removeItem(FEEDBACK_FORM_STATE)
+    }
 })
 
